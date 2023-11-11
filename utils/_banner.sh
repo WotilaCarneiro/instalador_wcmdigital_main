2b0e0cbe8a0dda7060ce231edf6daafbc1f5c321
WotilaCarneiro <wotila@gmail.com>
2023-11-10 22:19:56 -0300

    segundo commit

---
+++ utils/_banner.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+#
+# Print banner art.
+
+#######################################
+# Print a board. 
+# Globals:
+#   BG_BROWN
+#   NC
+#   WHITE
+#   CYAN_LIGHT
+#   RED
+#   GREEN
+#   YELLOW
+# Arguments:
+#   None
+#######################################
+print_banner() {
+
+  clear
+
+  printf "\n\n"
+
+printf "${CYAN_LIGHT}";
+
+
+
+printf ${CYAN_LIGHT}"                            WCM Digital - Sistemas de Automação                          \n";                                                                                                                                                           
+  
+  printf "${NC}";
+
+  printf "\n"
+}
